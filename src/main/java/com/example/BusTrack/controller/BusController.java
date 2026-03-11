5fd3c2b91e9572931e84a2346ba0612701c3b866
mmmichael-carvalho <162504810+mmmichael-carvalho@users.noreply.github.com>
2026-03-10 22:37:30 -0300

    upd

--- src/main/java/com/example/BusTrack/controller/BusController.java
+++ src/main/java/com/example/BusTrack/controller/BusController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/linhas")
-//@CrossOrigin(origins = "https://bustrackfront.onrender.com/")
+
 @CrossOrigin(origins = "http://localhost:4200")
 public class BusController {
 
